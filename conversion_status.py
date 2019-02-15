36687da983a294365f0a22d701da93021e505255
Seiji Emery <seijiemery@gmail.com>
2019-02-14 19:26:39 -0800

    added script to monitor completion of shrinkwrap / other processes

---
+++ conversion_status.py
@@ -0,0 +1,32 @@
+import sys
+import os
+import time
+
+def list_files (directory, file_ext = None, pred = None):
+    if file_ext and not pred:
+        pred = lambda file: file.endswith(file_ext)
+    
+    return [
+        os.path.join(dirpath, file)
+        for dirpath, subdirs, files in os.walk(directory)
+        for file in files
+        if pred and pred(file)
+    ]
+
+def show_file_conversion_status (import_dir, export_dir, import_pred, export_pred):
+    num_imports = len(list_files(import_dir, pred=import_pred))
+    num_exports = len(list_files(export_dir, pred=export_pred))
+    print("%s / %s"%(num_exports, num_imports))
+
+def run_periodic (dt, f, *args, **kwargs):
+    while True:
+        f(*args, **kwargs)
+        time.sleep(dt)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 3:
+        print("usage: %s <obj-imports> <obj-exports>")
+    else:
+        is_obj = lambda file: file.endswith('.obj')
+        run_periodic(1.0, lambda:
+            show_file_conversion_status(sys.argv[1], sys.argv[2], is_obj, is_obj))
